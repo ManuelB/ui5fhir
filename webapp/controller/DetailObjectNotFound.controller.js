2ce2d0e96498ff96208f72638ad335d883db5697
Manuel Blechschmidt <manuel.blechschmidt@gmx.de>
2023-03-23 15:00:24 +0000

    Added content of Application

---
+++ webapp/controller/DetailObjectNotFound.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+    "./BaseController"
+], function (BaseController) {
+    "use strict";
+
+    return BaseController.extend("ui5fhir.controller.DetailObjectNotFound", {});
+});
---
+++ webapp/test/integration/pages/Common.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/test/Opa5"
+], function(Opa5) {
+	"use strict";
+
+	return Opa5.extend("ui5fhir.test.integration.pages.Common", {
+
+	});
+
+});
